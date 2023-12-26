ff8472bcb014ce7e0683b3fad5d1cc868545babb
Aheige <aheige17@gmail.com>
2023-12-27 00:03:18 +0100

    Working, I think

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::net::{TcpListener, TcpStream};
-use tungstenite::{accept, protocol, client, WebSocket, Message};
-use rand::{self, Rng, seq::index};
+use tungstenite::{accept, WebSocket, Message};
+use rand::{self, Rng};
 use std::sync::{Arc, Mutex};
 use serde::{Serialize, ser::SerializeStruct};
 
@@ -12,14 +12,16 @@ fn main () {
     struct Client {
         id: u32,
         name: String,
+        ws: Arc<Mutex<WebSocket<TcpStream>>>
     }
 
     impl Client {
-        fn new(name: &str, id: u32) -> Self {
+        fn new(name: &str, id: u32, ws: Arc<Mutex<WebSocket<TcpStream>>>) -> Self {
            
             Self {
                 id,
-                name: name.to_string()
+                name: name.to_string(),
+                ws,
             }
         }
     }
@@ -42,21 +44,21 @@ fn main () {
         rng.gen_range(0..10000)
     }
 
-    let connected_clients: Arc<Mutex<Vec<Client>>> = Arc::new(Mutex::new(Vec::new()));
+    let connected_clients: Arc<Mutex<Vec<Arc<Mutex<Client>>>>> = Arc::new(Mutex::new(Vec::new()));
     
-    pub fn add_new_client(c: Client, connected_clients: Arc<Mutex<Vec<Client>>>) {
+    pub fn add_new_client(c: Arc<Mutex<Client>>, connected_clients: Arc<Mutex<Vec<Arc<Mutex<Client>>>>>) {
         println!("connected_client length before adding new Client: {}", connected_clients.lock().unwrap().len());
         connected_clients.lock().unwrap().push(c)
     }
 
-    pub fn remove_client(cid: u32, connected_clients: Arc<Mutex<Vec<Client>>>) {
+    pub fn remove_client(cid: u32, connected_clients: Arc<Mutex<Vec<Arc<Mutex<Client>>>>>) {
         println!("removing client");
         let mut clients = connected_clients.lock().unwrap();
-        
+
         println!("Amount of clients before removal: {}", clients.len());
 
         
-        if let Some(index) = clients.iter().position(|x| x.id == cid) {
+        if let Some(index) = clients.iter().position(|x| x.lock().unwrap().id == cid) {
             println!("Client removed with id: {}",  cid);
             clients.remove(index);
             println!("Amount of clients after removal: {}", clients.len());
@@ -66,18 +68,19 @@ fn main () {
         
     }
 
-    fn send_message_to_other_clients(sender_id: u32, message: Message, connected_clients: Arc<Mutex<Vec<Client>>>, websocket: &mut WebSocket<TcpStream>) {
-        let clients = connected_clients.lock().unwrap();
-        for client in clients.iter() {
+    fn send_message_to_other_clients(sender_id: u32, message: Message, connected_clients: Arc<Mutex<Vec<Arc<Mutex<Client>>>>>) {
+        let mut clients = connected_clients.lock().unwrap();
+        for client in clients.iter_mut() {
             let msg = message.clone();
-            if client.id != sender_id {
+            if client.lock().unwrap().id != sender_id {
                 
-                println!("Sending message to client: {}", client.id);
+                
+                // println!("Sending message to client: {:?}", [client.id, sender_id]);
                 
                 // let json_string = serde_json::to_string(message).expect("Failed to make JSON");
                 let send_object: tungstenite::Message = msg;
                 
-                websocket.send(send_object).expect("Ops")
+                client.lock().unwrap().ws.lock().unwrap().send(send_object).expect("Ops")
             }
         }
     }
@@ -86,12 +89,16 @@ fn main () {
     println!("Waiting for connections on url: {}", url);
     for stream in server.incoming() {
 
-        let client = Client::new("Name", generate_random_id());
-        println!("New client {}", client.id);
-        let connected_clients_clone = connected_clients.clone();
-        
+
+        let cid = generate_random_id();
+        let ws = Arc::new(Mutex::new(accept(stream.expect("Stream err")).expect("Could not accept new connections")));
+        let client = Arc::new(Mutex::new(Client::new("Name", cid, ws.clone())));
+        println!("New client {}", client.lock().unwrap().id);
+       
+        let cloned_client = client.clone();
+        let connected_clients_clone = connected_clients.clone(); // Clone again
         std::thread::spawn(move || {
-            let mut websocket = accept(stream.expect("Stream err")).expect("Could not accept new connections");
+
             let connected_clients_clone_inner = connected_clients_clone.clone(); // Clone again
             
             
@@ -99,21 +106,20 @@ fn main () {
 
 
             loop {
-                if websocket.can_read() {
-                    let msg = websocket.read().expect("Could not read");
+                if cloned_client.lock().unwrap().ws.lock().unwrap().can_read() {
+                    let msg = cloned_client.lock().unwrap().ws.lock().unwrap().read().expect("Could not read");
                     if msg.is_binary() || msg.is_text() {
                         // println!("{}", msg);
                         let connected_clients_list = connected_clients_clone.clone();
-                        let sender_id = connected_clients_clone.lock().unwrap().last().map(|c| c.id).unwrap_or_default();
-                        send_message_to_other_clients(sender_id,msg, connected_clients_list,&mut websocket);
+                        send_message_to_other_clients(cid,msg, connected_clients_list);
                         //    let send_object: tungstenite::Message = json_string.into();
                         //    websocket.send(send_object).expect("Could not send!")
                         
                     }
                 } else {
-                    let client_id = connected_clients_clone.lock().unwrap().last().map(|c| c.id).unwrap_or_default();
-                    println!("Connection closed by client with id, {}", client_id);
-                    remove_client(client_id, connected_clients_clone);
+                    
+                    println!("Connection closed by client with id, {}", cid);
+                    remove_client(cid, connected_clients_clone);
                     break;
                 }
             }
